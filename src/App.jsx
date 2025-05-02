83d17474891445d530a78e857eecd66560eb2add
nathanielpierson <nathanielepierson@gmail.com>
2025-05-02 11:01:28 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+<p>app init</p>
+    </div>
+  )
+}
+
+export default App
