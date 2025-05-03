578eec4169e700e5fe3321975b9b4f0725256055
nathanielpierson <nathanielepierson@gmail.com>
2025-05-03 19:44:51 -0400

    change api choice to help with learning RapidAPI

---
+++ SearchIndex.jsx
@@ -0,0 +1,10 @@
+export function SearchIndex ( {imageData} ) {
+  console.log("in SearchIndex")
+  console.log (imageData)
+  console.log("in SearchIndex")
+  return (
+    <div>
+      <p>search index</p>
+    </div>
+  )
+}
--- SearchPage.jsx
+++ SearchPage.jsx
@@ -1,19 +1,32 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { SearchIndex } from "/SearchIndex";
 
 export function SearchPage() {
   const [imageData, setImageData] = useState([]);
-  axios.get('rapidapi.com', {
-    params: {
-      App: 'default-application_10547932',
-      'X-RapidAPI-Key': 'value2',
-    }}).then(response => {
-      setImageData(response.data);
+  async function asyncDo() {
+
+    const options = {
+      method: 'GET',
+      url: 'https://imdb236.p.rapidapi.com/imdb/lowest-rated-movies',
+      headers: {
+        'x-rapidapi-key': '26c5b29a5dmshd3a5d2acddf0041p1df667jsn3d03d3010961',
+        'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await axios.request(options);
       console.log(response.data);
-    })
-  return(
-    <div>
-    <p>search page</p>
-    </div>
-  )
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  asyncDo();
+    return (
+      <div>
+      <SearchIndex imageData={imageData}/>
+      <p>search page</p>
+      </div>
+    )
 }
