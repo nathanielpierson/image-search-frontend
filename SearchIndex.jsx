bc84fc6ab6860327e39a0e86c281b26e749f01dd
nathanielpierson <nathanielepierson@gmail.com>
2025-05-03 20:13:34 -0400

    data passing added

--- SearchIndex.jsx
+++ SearchIndex.jsx
@@ -1,9 +1,14 @@
-export function SearchIndex ( {imageData} ) {
+export function SearchIndex ( {cast} ) {
   console.log("in SearchIndex")
-  console.log (imageData)
+  console.log (cast)
   console.log("in SearchIndex")
   return (
     <div>
+      {cast.map((cast) => (
+        <div key={cast.id}>
+        <p>{cast.fullName}</p>
+        </div>
+      ))}
       <p>search index</p>
     </div>
   )
--- SearchPage.jsx
+++ SearchPage.jsx
@@ -3,12 +3,12 @@ import { useState, useEffect } from "react";
 import { SearchIndex } from "/SearchIndex";
 
 export function SearchPage() {
-  const [imageData, setImageData] = useState([]);
+  const [castData, setCastData] = useState([]);
   async function asyncDo() {
 
     const options = {
       method: 'GET',
-      url: 'https://imdb236.p.rapidapi.com/imdb/lowest-rated-movies',
+      url: 'https://imdb236.p.rapidapi.com/imdb/tt1204342/cast',
       headers: {
         'x-rapidapi-key': '26c5b29a5dmshd3a5d2acddf0041p1df667jsn3d03d3010961',
         'x-rapidapi-host': 'imdb236.p.rapidapi.com'
@@ -18,6 +18,7 @@ export function SearchPage() {
     try {
       const response = await axios.request(options);
       console.log(response.data);
+      setCastData(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -25,7 +26,7 @@ export function SearchPage() {
   asyncDo();
     return (
       <div>
-      <SearchIndex imageData={imageData}/>
+      <SearchIndex cast={castData}/>
       <p>search page</p>
       </div>
     )
